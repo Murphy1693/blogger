7e810a846fbfb6ac09e96100cb6414e77f810d5a
Shannon Murphy <Murphy1693@gmail.com>
2023-02-25 06:32:05 -0800

    initial file structure

---
+++ src/app/SQL/CrashCourse/page.tsx
@@ -0,0 +1,3 @@
+export default function CrashCourse() {
+  return <div></div>;
+}
---
+++ src/app/SQL/layout.tsx
@@ -0,0 +1,3 @@
+export default function SQLLayout({ children }: { children: React.ReactNode }) {
+  return <div></div>;
+}
